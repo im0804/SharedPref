af800c3608e5bc6cfd927f0d165aa6e76be671a8
im0804 <95828796+im0804@users.noreply.github.com>
2023-01-26 11:09:58 +0200

    after java docs

--- app/src/main/java/com/example/sharedpref/MainActivity.java
+++ app/src/main/java/com/example/sharedpref/MainActivity.java
@@ -12,6 +12,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+/**
+ * saves information in a shared preference file
+ * @author Inbar Menahem
+ * @version 1.0
+ * @since 24/1/23
+ */
 public class MainActivity extends AppCompatActivity {
     TextView tvCount;
     EditText etName;
@@ -34,16 +40,34 @@ public class MainActivity extends AppCompatActivity {
         tvCount.setText("" + counter);
     }
 
+    /**
+     * Count.
+     * adds one to the counter and shows it on the text view.
+     *
+     * @param view the view
+     */
     public void count(View view) {
         counter += 1;
         tvCount.setText("" + counter);
     }
 
+    /**
+     * Reset.
+     * resets the counter and updates in on the text view.
+     *
+     * @param view the view
+     */
     public void reset(View view) {
         counter = 0;
         tvCount.setText("" + counter);
     }
 
+    /**
+     * Exit.
+     * closes the app.
+     *
+     * @param view the view
+     */
     public void exit(View view) {
         finish();
     }
--- app/src/main/java/com/example/sharedpref/credits.java
+++ app/src/main/java/com/example/sharedpref/credits.java
@@ -8,6 +8,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+/**
+ * Credits activity
+ */
 public class credits extends AppCompatActivity {
     TextView tvCredits;
 
